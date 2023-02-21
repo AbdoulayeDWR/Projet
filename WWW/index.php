d1156bd4a007c9bd07e34c71613e1943b16eff3c
AbdoulayeDWR <116828635+AbdoulayeDWR@users.noreply.github.com>
2023-02-21 17:58:13 +0100

    Add files via upload
    commit de toutes les pages de bases (propos deja faites)

---
+++ WWW/index.php
@@ -0,0 +1,17 @@
+<?php
+    include("../layout/header.html")
+?>
+    
+<section class="products">
+    <article class="big"></article>
+    <article class="small"></article>
+    <article class="small"></article>
+    <article class="small"></article>
+    <article class="small"></article>
+    <article class="small"></article>
+    <article class="small"></article>
+   
+</section>
+<?php
+    include("../layout/footer.html")
+?>
---
+++ WWW/propos.php
@@ -0,0 +1,53 @@
+<?php
+    include("../layout/header.html")
+?>    
+<main>
+  <div class="prez">
+      <h1>HEURE24</h1>
+      <p>The World's Watch Market</p>
+  </div>
+  <div class="prez">
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum assumenda accusantium velit commodi qui odit officiis voluptatem illo maiores similique molestias repellendus, natus optio ea facere. Harum iure facere voluptas?</p>
+  </div>
+  <div class="accordion" id="accordionPanelsStayOpenExample">
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+          Notre Histoire :
+        </button>
+      </h2>
+      <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+        <div class="accordion-body">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem esse modi beatae veniam ex recusandae aut, rerum tempora error laboriosam molestiae alias deserunt maxime? Aut inventore praesentium veniam quaerat id. Lorem ipsum dolor sit, amet consectetur adipisicing elit. At, soluta aperiam veniam quo inventore eveniet sed earum alias laborum minus. Ex sint, nostrum impedit quam odio ut rerum omnis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, perferendis nisi, pariatur hic facilis dolor soluta ratione perspiciatis odio a placeat? Veritatis exercitationem eos ea, assumenda nostrum corporis provident ipsum! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, quo minima eum distinctio voluptas voluptatibus sapiente vitae tempora doloremque culpa voluptatum hic ut, itaque modi quas, enim cumque adipisci eos.</p>
+        </div>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+          Notre equipe :
+        </button>
+      </h2>
+      <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+        <div class="accordion-body">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, enim sed quae, non suscipit odit fugiat dolorem voluptatem ullam molestiae reprehenderit excepturi alias voluptate cum minima possimus quidem similique. Optio. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, voluptate quasi perferendis mollitia saepe nobis et assumenda beatae hic nesciunt obcaecati possimus non quibusdam expedita, consequuntur veritatis eum omnis at.</p>
+        </div>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+          Nos produits :
+        </button>
+      </h2>
+      <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+        <div class="accordion-body">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, eveniet sunt vero perspiciatis provident vitae corrupti fugit eum quae sit ab delectus illum voluptatibus aut, velit dolore corporis, repudiandae labore. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa reiciendis quas ducimus veritatis totam, repellat, libero atque maxime nostrum veniam necessitatibus numquam maiores facere, id expedita earum illum aut debitis.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+<?php
+  include("../layout/footer.html")
+?>
