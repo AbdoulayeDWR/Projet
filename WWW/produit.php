b96b1474aec5b57214cc5d33d363a1ff05b2fd88
gua <74402918+Guacax@users.noreply.github.com>
2023-02-21 18:02:29 +0100

    Add files via upload
    premier jet de la page produits

---
+++ WWW/produit.php
@@ -0,0 +1,23 @@
+<?php include("layout/header.php"); ?>
+<main>
+    <div >
+        <img id="big" src="../img/testpics/Fhdp0vkWQAILshO.jpg" alt="big_pic">
+        <div>
+            <img id="tiny" src="../img/testpics/caraclope.png" alt="tiny_pics">
+            <img id="tiny" src="../img/testpics/caraclope.png" alt="tiny_pics">
+            <img id="tiny" src="../img/testpics/caraclope.png" alt="tiny_pics">
+            <img id="tiny" src="../img/testpics/caraclope.png" alt="tiny_pics">
+        </div>
+    </div>
+
+    <section>
+        <h1>1972 TUDOR MONTE CARLO 7169/0</h1>
+
+        <p>Le Tudor Monte Carlo, le " Paul Newman " du pauvre... Poor Man's Paul Newman en VO.
+
+        Le " Monte Carlo " aussi surnommé : le " Paul Newman " du pauvre, car ne coûtant qu'un dixième / neuvième / huitième /septième / sixième... Du " Paul Newman"
+        </p>
+    </section>
+</main>
+
+<?php include("layout/footer.php"); ?>
