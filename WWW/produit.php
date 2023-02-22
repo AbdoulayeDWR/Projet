117f5e3e4d1ac27f92f60b43055b24480dd53760
gua <74402918+Guacax@users.noreply.github.com>
2023-02-22 17:19:02 +0100

    Add files via upload

--- WWW/produit.php
+++ WWW/produit.php
@@ -1,23 +1,51 @@
 <?php include("layout/header.php"); ?>
 <main>
-    <div >
+    <div id="photos">
         <img id="big" src="../img/testpics/Fhdp0vkWQAILshO.jpg" alt="big_pic">
-        <div>
-            <img id="tiny" src="../img/testpics/caraclope.png" alt="tiny_pics">
-            <img id="tiny" src="../img/testpics/caraclope.png" alt="tiny_pics">
-            <img id="tiny" src="../img/testpics/caraclope.png" alt="tiny_pics">
-            <img id="tiny" src="../img/testpics/caraclope.png" alt="tiny_pics">
+        <div id="tiny">
+            <img src="../img/testpics/caraclope.png" alt="tiny_pics">
+            <img src="../img/testpics/caraclope.png" alt="tiny_pics">
+            <img src="../img/testpics/caraclope.png" alt="tiny_pics">
+            <img src="../img/testpics/caraclope.png" alt="tiny_pics">
         </div>
     </div>
 
     <section>
-        <h1>1972 TUDOR MONTE CARLO 7169/0</h1>
-
-        <p>Le Tudor Monte Carlo, le " Paul Newman " du pauvre... Poor Man's Paul Newman en VO.
-
-        Le " Monte Carlo " aussi surnommé : le " Paul Newman " du pauvre, car ne coûtant qu'un dixième / neuvième / huitième /septième / sixième... Du " Paul Newman"
-        </p>
+        <div>
+            <h1>1972 TUDOR MONTE CARLO 7169/0</h1>
+            <p>Le Tudor Monte Carlo, le " Paul Newman " du pauvre... Poor Man's Paul Newman en VO.
+            <br><br>
+            Le " Monte Carlo " aussi surnommé : le " Paul Newman " du pauvre, car ne coûtant qu'un dixième / neuvième / huitième /septième / sixième... Du " Paul Newman"
+            </p>
+            <br>
+            <h2>Prix</h2>
+            <button id="buy">Ajouter au panier</button>
+            <br><br>
+            <h3>Caractéristiques</h3>
+        </div>
+        
+        <table>
+            <tr>
+                <th>Réference</th>
+                <td>7169/0</td>
+            </tr>
+            <tr>
+                <th>Mouvement</th>
+                <td>Remontage automatique</td>
+            </tr>
+            <tr>
+                <th>Boitier</th>
+                <td>Acier</td>
+            </tr>
+            <tr>
+                <th>Bracelet</th>
+                <td>Acier</td>
+            </tr>
+            <tr>
+                <th>Année</th>
+                <td>1972</td>
+            </tr>
+        </table>
     </section>
 </main>
-
 <?php include("layout/footer.php"); ?>
