cb53ee4c1e03eb2714a9424bef8140262f6db4fc
kao-outar <122811747+kao-outar@users.noreply.github.com>
2023-02-22 15:49:02 +0100

    Add files via upload
    la page index en php

--- projet/index.php
+++ projet/index.php
@@ -1,6 +1,133 @@
 <?php
     include("layout/header.html")
 ?>
+<main>
+            <article class="big">
+                <article>
+                <div class="image-text">
+                            <H2 id="image-title">L'heure du luxe</H2>
+                            <p>Heure24 est la plateforme plébiscitée par plus de 1500 vendeurs professionnels de montres et plusieurs milliers de vendeurs privés. Plus de 6 millions de visiteurs par mois s'intéressent aux montres de marque du monde entier. Bienvenue chez Heure 24 !</p>
+                        </div>
+                </article>
+            </article>
+            <article>
+                <div>
+                <img src="media/images/H1.png" alt="" width="280px">
+                </div>
+                <div>
+                    <p>
+                        OMEGA SPEEDMASTER
+                         
+                        <br>
+                        <b>100 €</b>
+                    </p>
+                </div>
+            </article>
+            <article>
+                <div>
+                    <img src="media/images/h2.jpg" alt="" width="280px">
+                    </div>
+                    <div>
+                        <p>
+                            OMEGA SPEEDMASTER
+                             
+                            <br>
+                            <b>100 €</b>
+                        </p>
+                    </div>
+            </article>
+            <article>
+                <div>
+                    <img src="media/images/H1.png" alt="" width="280px">
+                    </div>
+                    <div>
+                        <p>
+                            OMEGA SPEEDMASTER
+                            <br>
+                            <b>100 €</b>
+                        </p>
+                    </div>
+            </article>
+            <article>
+                <div>
+                    <img src="media/images/h2.jpg" alt="" width="280px">
+                    </div>
+                    <div>
+                        <p>
+                            OMEGA SPEEDMASTER
+                             
+                            <br>
+                            <b>100 €</b>
+                        </p>
+                    </div>
+            </article>
+            <article>
+                <div>
+                    <img src="media/images/H1.png" alt="" width="280px">
+                    </div>
+                    <div>
+                        <p>
+                            OMEGA SPEEDMASTER
+                            
+                            <br>
+                            <b>100 €</b>
+                        </p>
+                    </div>
+            </article>
+              <article>
+                <div>
+                    <img src="media/images/h2.jpg" alt="" width="280px">
+                    </div>
+                    <div>
+                        <p>
+                            OMEGA SPEEDMASTER
+                             
+                            <br>
+                            <b>100 €</b>
+                        </p>
+                    </div>
+            </article>
+    </main>
+    <!-- <section class="products">
+        <article class="big">
+            <div class="cover">
+                <img src="media/images/cover3.jpg" alt="cover" width="200px">
+            </div>
+        </article>
+        <article class="small">
+            <div>
+                <img src="media/images/1.jpg" alt="" width="280px">
+            </div>
+        </article>
+        <article class="small">
+            <div>
+                <img src="media/images/2.jpg" alt="" width="280px">
+            </div>
+        </article>
+        <article class="small">
+            <div>
+                <img src="media/images/3.jpg" alt="" width="280px">
+            </div>
+        </article>
+        <article class="small">
+            <div>
+                <img src="media/images/4.jpg" alt="" width="280px">
+            </div>
+        </article>
+        <article class="small">
+            <div>
+                <img src="media/images/5.jpg" alt="" width="280px">
+            </div>
+        </article>
+        <article class="small">
+            <div>
+                <img src="media/images/6.jpg" alt="" width="280px">
+            </div>
+        </article> -->
+        
+        
+       
+    </section>
 <?php
     include("layout/footer.html")
 ?>
