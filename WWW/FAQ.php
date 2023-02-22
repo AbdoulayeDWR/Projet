42e6ffdca486214ca7aede4cdb3d71874a84edda
AbdoulayeDWR <116828635+AbdoulayeDWR@users.noreply.github.com>
2023-02-23 00:23:12 +0100

    Update FAQ.php
    commit du FAQ.php finalise

--- WWW/FAQ.php
+++ WWW/FAQ.php
@@ -1,3 +1,70 @@
 <?php
     include("../layout/header.html")
 ?>
+<main>
+    <h2>FAQ : Les questions posées fréquemment</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolores praesentium eligendi quam quaerat officia adipisci! Illum harum nesciunt qui accusamus ipsa dignissimos, laborum quasi eligendi debitis? Officia, asperiores qui.</p>
+    <div class="accordion accordion-flush" id="accordionFlushExample">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingOne">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                    D'ou viennent vos produit ?
+                </button>
+            </h2>
+            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor asperiores ipsum quisquam commodi libero dolore ad officia similique soluta, voluptate provident dignissimos aut voluptatem corrupti officiis illum, nam earum dicta.</div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingTwo">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                    Est-ce fiable a travers le temps ?
+                </button>
+            </h2>
+            <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae quis doloremque magnam, dignissimos harum provident fugiat obcaecati reiciendis voluptatum quo vel facilis nemo incidunt consequuntur commodi sapiente quam. Officia, odit.</div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingThree">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                    Qui sommes nous ?
+                </button>
+            </h2>
+            <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, exercitationem officiis similique dignissimos quidem eaque cupiditate hic. Adipisci facilis voluptas necessitatibus voluptatibus nemo consectetur, cupiditate consequatur, esse quod maxime illo! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate enim voluptas totam quia, nisi perspiciatis. Accusamus totam ea delectus corrupti beatae ut nobis ipsam, excepturi eius porro facere dolorum quibusdam. Pour plus de details cliquez <a href="propos.php">ici</a>.</div>
+            </div>
+        </div>
+    </div>
+    <div class="montre">
+        <img src="../media/bahamas.jpg" alt="">
+        <div class="text_montre">
+            <h3>Lorem ipsum dolor</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores totam tempore eius itaque sunt deleniti neque maiores ab, consequatur atque mollitia placeat, accusamus deserunt. Sit doloribus quas unde delectus. Nostrum!</p>
+        </div>
+    </div>
+    <div class="montre_d">
+        <div class="text_montre">
+            <h3>Lorem ipsum dolor</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores totam tempore eius itaque sunt deleniti neque maiores ab, consequatur atque mollitia placeat, accusamus deserunt. Sit doloribus quas unde delectus. Nostrum!</p>
+        </div>
+        <img src="../media/bahamas.jpg" alt="">
+    </div>
+    <div class="montre">
+        <img src="../media/bahamas.jpg" alt="">
+        <div class="text_montre">
+            <h3>Lorem ipsum dolor</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores totam tempore eius itaque sunt deleniti neque maiores ab, consequatur atque mollitia placeat, accusamus deserunt. Sit doloribus quas unde delectus. Nostrum!</p>
+        </div>
+    </div>
+    <div class="montre_d">
+        <div class="text_montre">
+            <h3>Lorem ipsum dolor</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores totam tempore eius itaque sunt deleniti neque maiores ab, consequatur atque mollitia placeat, accusamus deserunt. Sit doloribus quas unde delectus. Nostrum!</p>
+        </div>
+        <img src="../media/bahamas.jpg" alt="">
+    </div>
+</main>
+<?php
+    include("../layout/footer.html")
+?>
